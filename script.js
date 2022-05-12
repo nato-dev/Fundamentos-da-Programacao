a66cdacc7d918410f6e99151d19f42fe19fca1f2
PiperSuperCub <103214780+PiperSuperCub@users.noreply.github.com>
2022-05-12 18:52:37 -0300

    Add files via upload
    Learning "If" with Exercises using JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+var a = 10000
+var b = 10000;
+if (a != b) {
+    var mensagem = "A é diferente de B";
+    alert(mensagem);
+}
+
+if (a = b) {
+    var mensagem = "A é igual a B";
+    alert (mensagem);
+}
